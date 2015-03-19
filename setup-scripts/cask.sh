26791c0db741a02fb7b0361ec382d4631c9d3751
Ross McKelvie <iam@rossmckelvie.com>
2015-03-19 13:15:48 -0500

    add 1password, dash, github, mamp, sequel-pro, and slack

--- setup-scripts/cask.sh
+++ setup-scripts/cask.sh
@@ -6,11 +6,17 @@ function installcask() {
 	brew cask install "${@}" 2> /dev/null
 }
 
+installcask 1password
 installcask atom
+installcask dash
 installcask dropbox
+installcask github
 installcask google-chrome
 installcask iterm2
 installcask macvim
+installcask mamp
 installcask nosleep
 installcask the-unarchiver
+installcask sequel-pro
+installcask slack
 installcask vlc
