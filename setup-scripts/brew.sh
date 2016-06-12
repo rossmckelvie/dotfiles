43595335e4460253689a50c670d0088318e87f16
Ross McKelvie <iam@rossmckelvie.com>
2016-06-12 17:46:56 -0500

    only run brew scripts on os x

--- setup-scripts/brew.sh
+++ setup-scripts/brew.sh
@@ -1,11 +1,13 @@
-#Install Hombrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew doctor
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  #Install Hombrew
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew doctor
 
-# Install useful binaries
-brew install curl
-brew install node
-brew install the_silver_searcher
-brew install wget
-brew install zsh
-brew install z
+  # Install useful binaries
+  brew install curl
+  brew install node
+  brew install the_silver_searcher
+  brew install wget
+  brew install zsh
+  brew install z
+fi
--- setup-scripts/cask.sh
+++ setup-scripts/cask.sh
@@ -1,27 +1,25 @@
-# Install native apps
-brew tap phinze/homebrew-cask
-brew install brew-cask
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # Install native apps
+  brew tap phinze/homebrew-cask
+  brew install brew-cask
 
-# Core / Personal
-brew cask install 1password
-brew cask install alfred
-brew cask install dropbox
-brew cask install google-chrome
-brew cask install istat-menus
-brew cask install nosleep
-brew cask install mailbox
-brew cask install mou
-brew cask install slack
-brew cask install spotify
-brew cask install the-unarchiver
-brew cask install vlc
+  # Core / Personal
+  brew cask install 1password
+  brew cask install alfred
+  brew cask install dropbox
+  brew cask install google-chrome
+  brew cask install istat-menus
+  brew cask install mailbox
+  brew cask install mou
+  brew cask install slack
+  brew cask install spotify
+  brew cask install the-unarchiver
+  brew cask install vlc
 
-# Development
-brew cask install atom
-brew cask install dash
-brew cask install fabric
-brew cask install github
-brew cask install iterm2
-brew cask install macvim
-brew cask install mamp
-brew cask install sequel-pro
+  # Development
+  brew cask install atom
+  brew cask install dash
+  brew cask install fabric
+  brew cask install github
+  brew cask install iterm2
+fi
